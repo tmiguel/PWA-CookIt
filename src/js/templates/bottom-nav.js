07ef5dce1f5f9629f81c12526e2024b90dd5498e
Tiago Miguel <tiagomfmiguel@gmail.com>
2025-12-12 06:43:11 +0000

    Create bottom-nav.js

---
+++ src/js/templates/bottom-nav.js
@@ -0,0 +1,16 @@
+export const bottomNavTemplate = `
+    <nav class="bottom-nav">
+        <button class="bottom-nav__item">
+            <span class="bottom-nav__icon">🏠</span>
+            <span>Início</span>
+        </button>
+        <button class="bottom-nav__item">
+            <span class="bottom-nav__icon">📖</span>
+            <span>Receitas</span>
+        </button>
+        <button class="bottom-nav__item">
+            <span class="bottom-nav__icon">👤</span>
+            <span>Perfil</span>
+        </button>
+    </nav>
+`;
