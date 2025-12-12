fce4300516c64dd5330dd697b11021703151cee7
Tiago Miguel <tiagomfmiguel@gmail.com>
2025-12-12 21:45:03 +0000

    Update header.js

--- src/js/templates/header.js
+++ src/js/templates/header.js
@@ -1,6 +1,11 @@
 export const headerTemplate = `
     <header class="header">
-        <div class="header__logo">CookIt 🍳</div>
+        <div class="header__brand">
+            <!-- Logótipo pequeno -->
+            <img src="assets/Icon-192x192.png" alt="CookIt" class="header__icon">
+            <div class="header__logo">CookIt</div>
+        </div>
+        
         <button id="header-btn-logout" class="header__logout" title="Terminar Sessão">
             <svg width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
                 <path d="M9 21H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h4"></path>
