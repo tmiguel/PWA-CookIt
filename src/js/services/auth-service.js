b3c2fe19a5517403024f5a9ffcc5a10fe7bee61b
Tiago Miguel <tiagomfmiguel@gmail.com>
2025-12-12 19:44:32 +0000

    Update auth-service.js

--- src/js/services/auth-service.js
+++ src/js/services/auth-service.js
@@ -2,16 +2,28 @@ import { auth, googleProvider } from '../firebase.js';
 import { 
     signInWithRedirect, 
     signOut, 
-    onAuthStateChanged
+    onAuthStateChanged,
+    getRedirectResult // <--- Importante
 } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
 
+// Iniciar Login
 export async function loginWithGoogle() {
     try {
-        // Usa Redirect (Melhor para Telemóvel)
         await signInWithRedirect(auth, googleProvider);
     } catch (error) {
-        console.error("Erro Login:", error);
-        alert("Erro ao iniciar: " + error.message);
+        console.error("Erro no redirect:", error);
+        alert("Erro: " + error.message);
+    }
+}
+
+// Finalizar Login (Processar regresso)
+export async function finishRedirectLogin() {
+    try {
+        // O Firebase verifica se viemos do Google agora
+        await getRedirectResult(auth);
+    } catch (error) {
+        console.error("Erro ao voltar do Google:", error);
+        alert("Falha na autenticação: " + error.message);
     }
 }
 
