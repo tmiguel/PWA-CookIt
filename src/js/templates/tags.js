ffed9f04fefd296ea90d8cb484e073a68874832a
Tiago Miguel <tiagomfmiguel@gmail.com>
2025-12-13 16:56:22 +0000

    Update tags.js

--- src/js/templates/tags.js
+++ src/js/templates/tags.js
@@ -2,10 +2,7 @@ export const tagsTemplate = `
 <div class="page-content">
     <div class="form-header">
         <button id="btn-back-settings" class="btn-back">
-            <!-- Ícone SVG Chevron Left -->
-            <svg width="28" height="28" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
-                <polyline points="15 18 9 12 15 6"></polyline>
-            </svg>
+            <svg width="28" height="28" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><polyline points="15 18 9 12 15 6"></polyline></svg>
         </button>
         <h2 class="page-title">Gerir Tags</h2>
     </div>
@@ -15,12 +12,10 @@ export const tagsTemplate = `
             <input type="text" id="input-code" class="input-field input-code" placeholder="CÓD" maxlength="5">
             <input type="text" id="input-name" class="input-field" placeholder="Nome (ex: Pequeno Almoço)">
         </div>
-        <!-- Botão Discreto -->
-        <button id="btn-add" class="btn-discrete">Adicionar</button>
+        <button id="btn-add" class="btn-discrete">Adicionar Tag</button>
         <p id="error-msg" style="color:var(--danger-color); text-align:center; margin-top:15px; font-size:0.9rem; display:none;"></p>
     </div>
 
-    <!-- Subtítulo Alterado -->
     <h3 class="list-subtitle">Tags</h3>
     <ul id="list-container" class="manage-list"></ul>
 </div>
